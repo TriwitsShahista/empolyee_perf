9410074fb79e7995a0a933ca31bc4d95f24a9e13
TriwitsShahista <121780268+TriwitsShahista@users.noreply.github.com>
2023-04-27 18:21:57 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
+import EmpForm from "./components/EmpForm";
 import EmpReg from "./components/EmpReg";
+import EmpTable from "./components/EmpTable";
+import SideBar from "./components/SideBar";
+import EmpPrfTable from "./components/EmpPrfTable";
 
 function App() {
 
   return (
     <>
     <BrowserRouter>
+    <SideBar/>
     <Routes>
-      <Route exact path="/" element={<EmpReg/>}/>
+      <Route exact path="/empForm" element={<EmpForm/>}/>
+      <Route exact path="/empReg" element={<EmpReg/>}/>
+      <Route exact path="/empPrfTable" element={<EmpPrfTable/>}/>
+      <Route exact path="/empTable" element={<EmpTable/>}/>
       </Routes>
     </BrowserRouter>
     </>
